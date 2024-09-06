f26aa6ea366de8c90eb41b19e2fd5caa67fb39d8
Pedro Simão <pedrosimaocontato@gmail.com>
2024-09-06 11:45:51 -0300

    finalizado o projeto do imersão dev

--- js/dados.js
+++ js/dados.js
@@ -1,63 +1,20 @@
 let dados = [
-    
-     {
-        nome: "Lee Sang-hyeok (Faker)",
-        descricao: "Considerado o maior jogador de League of Legends de todos os tempos, Faker é conhecido por suas mecânicas impecáveis e sua mente estratégica. É tetracampeão mundial.",
-        linkWikipedia: "https://pt.wikipedia.org/wiki/Faker_(jogador)",
-         mundiais: 4
+    {
+        titulo: "Rayssa Leal",
+        descricao: "Rayssa Leal, a Fadinha, é uma skatista brasileira que conquistou o mundo com seu talento e carisma. Aos poucos anos, ela já coleciona títulos e inspira milhões de jovens.",
+        link: "https://pt.wikipedia.org/wiki/Rayssa_Leal",
+        tags: "skate fada sk8"
     },
     {
-        nome: "Bae Jun-sik (Bang)",
-        descricao: "Ex-atirador da SKT T1, Bang conquistou diversos títulos ao lado de Faker, incluindo três campeonatos mundiais. Conhecido por sua precisão e calma sob pressão.",
-        linkWikipedia: "https://pt.wikipedia.org/wiki/Bae_Jun-sik",
-        mundiais: 3
+        titulo: "Beatriz Souza",
+        descricao: "Beatriz Souza é uma judoca brasileira que se destacou nos Jogos Olímpicos de Paris 2024, conquistando a medalha de ouro e inspirando novas gerações de atletas.",
+        link: "https://pt.wikipedia.org/wiki/Beatriz_Souza",
+        tags: "judô judo judoca judoka"
     },
     {
-        nome: "Jian Zihao (Uzi)",
-        descricao: "Um dos maiores ADCs da história do League of Legends, Uzi é conhecido por suas mecânicas incríveis e por ser um carregador nato. Apesar de não ter conquistado o título mundial, chegou em duas finais.",
-        linkWikipedia: "https://pt.wikipedia.org/wiki/Jian_Zihao",
-        mundiais: 0
-    },
-    {
-        nome: "Kim Geon-bu (Canyon)",
-        descricao: "Jungler sul-coreano, conhecido por seu estilo de jogo agressivo e suas jogadas inteligentes. Foi campeão mundial em 2020 com a equipe DWG KIA.",
-        linkWikipedia: "https://pt.wikipedia.org/wiki/Kim_Geon-bu",
-        mundiais: 1
-    },
-    {
-        nome: "Park Jae-hyuk (Ruler)",
-        descricao: "Atirador sul-coreano, conhecido por sua consistência e por ser um dos melhores jogadores de sua posição. Foi campeão mundial em 2022 com a equipe DRX.",
-        linkWikipedia: "https://pt.wikipedia.org/wiki/Park_Jae-hyuk",
-        mundiais: 1
-    },
-    {
-        nome: "Felipe  Gonçalves (brTT)",
-        descricao: "Considerado um dos maiores jogadores de League of Legends do Brasil, brTT é conhecido por sua experiência e liderança. Foi multicampeão do CBLoL e se destacou no cenário internacional.",
-        linkWikipedia: "https://pt.wikipedia.org/wiki/BrTT",
-        mundiais: 0
-    },
-    {
-        nome: "Pyosik",
-        descricao: "Jungler sul-coreano, conhecido por seu estilo de jogo agressivo e suas mecânicas impecáveis. Foi campeão mundial em 2020 com a equipe DWG KIA, ao lado de Canyon.",
-        linkWikipedia: "Não tem" , 
-        mundiais: 1
-    },
-    {
-        nome:"Caps",
-        descricao: "Mid laner dinamarquês, conhecido por sua versatilidade e capacidade de jogar com diversos campeões. Conquistou diversos títulos na LEC e MSI.",
-        linkWikipedia: "https://en.wikipedia.org/wiki/Caps_(gamer)",
-        mundiais: 0 // Verificar se conquistou algum mundial
-    },
-    {
-        nome: "Cho " + "Chovy" + " Jae-won",
-        descricao: "Considerado um dos maiores mid laners da atualidade, Chovy é conhecido por suas mecânicas impecáveis e seu estilo de jogo agressivo. Já passou por diversas equipes e conquistou diversos títulos.",
-        linkWikipedia: "https://en.wikipedia.org/wiki/Chovy_(gamer)",
-        mundiais: 0 // Verificar se conquistou algum mundial
-    },
-    {
-        nome:"Showmaker",
-        descricao: "Mid laner sul-coreano, conhecido por sua calma e consistência. Foi campeão mundial em 2020 com a equipe DWG KIA.",
-        linkWikipedia: "Não tem",
-        mundiais: 1
+        titulo: "Rebeca Andrade",
+        descricao: "Rebeca Andrade é uma ginasta brasileira que conquistou o mundo com suas acrobacias e força. Aos poucos anos, ela já coleciona títulos e é considerada uma das maiores ginastas da história do Brasil.",
+        link: "https://pt.wikipedia.org/wiki/Rebeca_Andrade",
+        tags: "ginástica ginastica ouro"
     }
 ];
